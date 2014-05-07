494cd77dbeef338a2f4050e088319b817ade03d2
IgorsSivickis <mahmon@inbox.lv>
2014-05-07 15:53:15 +0300

    upd

--- Attributor/AttViewController.m
+++ Attributor/AttViewController.m
@@ -9,10 +9,32 @@
 #import "AttViewController.h"
 
 @interface AttViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *body;
+@property (weak, nonatomic) IBOutlet UILabel *headLine;
 
 @end
 
 @implementation AttViewController
+- (IBAction)changeBodySelectionColorToMatchBackgroundOfButton:(UIButton *)sender
+{
+        [self.body.textStorage addAttribute:NSForegroundColorAttributeName
+                                      value:sender.backgroundColor
+                                      range:self.body.selectedRange];
+}
+
+
+- (IBAction)outlineBodySelection:(id)sender
+{
+    [self.body.textStorage addAttributes:@{ NSStrokeWidthAttributeName: @-3,
+                                            NSStrokeColorAttributeName: [UIColor blackColor]} range:self.body.selectedRange];
+}
+
+- (IBAction)unoutlineBodySelection:(id)sender
+{
+    [self.body.textStorage removeAttribute:NSStrokeWidthAttributeName
+                                     range:self.body.selectedRange];
+}
+
 
 - (void)viewDidLoad
 {
@@ -20,10 +42,6 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 @end
